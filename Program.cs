83807c074bd0acdd131c58458a12479d6fc12495
Malik950 <54435682+Malik950@users.noreply.github.com>
2020-11-18 08:10:29 +0100

    klar

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace felhantering1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int resultat;
+            if (!TryParse(Console.ReadLine(), out resultat))
+                Console.WriteLine("Skriva in ett tal");
+        }
+
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return  true;
+            }
+
+            catch
+            {
+                result = 0;
+                return  false;
+            }
+        }
+    }
+}
